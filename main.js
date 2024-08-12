e398956a2fe9be02abbb3cf074e0d5c786c0db2f
Sanya Hussain <sanz_133@hotmail.com>
2024-08-12 16:32:10 +0100

    added typescript

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 "use strict";
+const buttons = document.querySelectorAll("button");
+const inputDiv = document.getElementsByClassName("input-screen");
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        console.log(button.innerText);
+        event.preventDefault();
+    });
+});
--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-const getButtons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll("button");
+const inputDiv = document.getElementById("input-txt") as HTMLDivElement;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (event: Event) => {
+    console.log(button.innerText);
+    event.preventDefault();
+  });
+});
