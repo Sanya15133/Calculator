e6c49ff0fcb24a81d3b3f2767b835ba90c9b3325
Sanya Hussain <sanz_133@hotmail.com>
2024-08-14 11:16:58 +0100

    calculator works, just need to deal with edge cases and a better clear button

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 "use strict";
-const buttons = document.querySelectorAll("buttons");
 const inputDiv = document.getElementById("input-txt");
-const operators = ["+", "-", "*", "/", "="];
+const operators = ["+", "-", "*", "/"];
 let result = 0;
 let isNumber = false;
 let currentOperator;
@@ -9,13 +8,13 @@ const clearButton = document.getElementById("clear");
 clearButton.addEventListener("click", (event) => {
     event.preventDefault();
     inputDiv.innerText = "";
+    return;
 });
+const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
         event.preventDefault();
-        inputDiv.innerText += button.innerHTML;
-        let sum = inputDiv.innerHTML;
-        console.log(sum);
+        inputDiv.innerText += button.innerText;
     });
 });
 const solveButton = document.getElementById("solve");
@@ -33,3 +32,22 @@ const calculateSum = (a, b, operator) => {
         return a - b;
     }
 };
+solveButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    let sum = inputDiv.innerHTML;
+    let splitSum;
+    for (let i = 0; i < operators.length; i++) {
+        currentOperator = operators[i];
+        splitSum = sum.split(operators[i]);
+        if (splitSum[1]) {
+            let num1 = parseInt(splitSum[0]);
+            let removeEquals = splitSum[1].replace("=", "");
+            let num2 = parseInt(removeEquals);
+            let result = calculateSum(num1, num2, currentOperator);
+            if (result) {
+                let resultToString = result === null || result === void 0 ? void 0 : result.toString();
+                inputDiv.innerText = resultToString;
+            }
+        }
+    }
+});
--- main.ts
+++ main.ts
@@ -1,25 +1,22 @@
-const buttons = document.querySelectorAll("buttons");
-
 const inputDiv = document.getElementById("input-txt") as HTMLDivElement;
 
-const operators = ["+", "-", "*", "/", "="];
+const operators = ["+", "-", "*", "/"];
 let result = 0;
 let isNumber = false;
 let currentOperator;
 
 const clearButton = document.getElementById("clear") as HTMLButtonElement;
-
 clearButton.addEventListener("click", (event: Event) => {
   event.preventDefault();
   inputDiv.innerText = "";
+  return;
 });
 
+const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", (event: Event) => {
     event.preventDefault();
-    inputDiv.innerText += button.innerHTML;
-    let sum = inputDiv.innerHTML;
-    console.log(sum);
+    inputDiv.innerText += button.innerText;
   });
 });
 
@@ -36,3 +33,23 @@ const calculateSum = (a: number, b: number, operator: string) => {
     return a - b;
   }
 };
+
+solveButton.addEventListener("click", (event: Event) => {
+  event.preventDefault();
+  let sum = inputDiv.innerHTML;
+  let splitSum;
+  for (let i = 0; i < operators.length; i++) {
+    currentOperator = operators[i];
+    splitSum = sum.split(operators[i]);
+    if (splitSum[1]) {
+      let num1 = parseInt(splitSum[0]);
+      let removeEquals = splitSum[1].replace("=", "");
+      let num2 = parseInt(removeEquals);
+      let result = calculateSum(num1, num2, currentOperator);
+      if (result) {
+        let resultToString = result?.toString();
+        inputDiv.innerText = resultToString;
+      }
+    }
+  }
+});
